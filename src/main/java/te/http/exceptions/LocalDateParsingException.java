aa04a0dd3db6014c2dcedb730d381eb8c64ff3a1
Todd Elvers <toddelvers@carfax.com>
2018-08-31 13:55:21 -0500

    Improved exception message

--- src/main/java/te/http/exceptions/LocalDateParsingException.java
+++ src/main/java/te/http/exceptions/LocalDateParsingException.java
@@ -8,7 +8,8 @@ import te.http.serialization.domain.LocalDateFormat;
 
 public class LocalDateParsingException extends RuntimeException {
     private static final String EXCEPTION_MESSAGE_SUFFIX = "To customize the deserialization behavior create your own implementation " +
-            "of MultiFormatLocalDateDeserializer and register it instead (see JsonMarshalling for more details).";
+            "of MultiFormatLocalDateDeserializer and register it instead (see JsonMarshalling " +
+            "for how to register a TypeAdapter for GSON).";
 
     public LocalDateParsingException(String input, Collection<LocalDateFormat> formatsTried) {
         super(
