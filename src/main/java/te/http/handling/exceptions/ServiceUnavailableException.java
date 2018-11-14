d4158bf38af7b11fa2d5ce17a365fe2e31489481
Todd Elvers <toddelvers@carfax.com>
2018-11-13 20:09:32 -0600

    Added support for detecting read timeouts & connect timeouts
    Converted integration tests to unit tests by using mockserver

--- src/main/java/te/http/handling/exceptions/ServiceUnavailableException.java
+++ src/main/java/te/http/handling/exceptions/ServiceUnavailableException.java
@@ -1,10 +1,14 @@
 package te.http.handling.exceptions;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.Optional;
 
 import okhttp3.Request;
 
 public class ServiceUnavailableException extends IOException {
+    private static final String READ_TIMEOUT_TEXT = "Read timed out";
+    private static final String CONNECT_TIMEOUT_TEXT = "connect timed out";
 
     private Request request;
     private Throwable rootCause;
@@ -29,5 +33,40 @@ public class ServiceUnavailableException extends IOException {
 
     public Throwable getRootCause() {
         return rootCause;
+    }
+
+
+    /**
+     * @return true if, and only if, the exception thrown is a {@link SocketTimeoutException}
+     * whose text exactly matches {@link #CONNECT_TIMEOUT_TEXT}.
+     */
+    public boolean wasConnectTimeout() {
+        if (rootCause instanceof SocketTimeoutException) {
+            return Optional.of(getRootCause())
+                    .map(Throwable::fillInStackTrace)
+                    .map(Throwable::getMessage)
+                    .map(msg -> msg.contains(CONNECT_TIMEOUT_TEXT))
+                    .orElse(false);
+        }
+
+        return false;
+    }
+
+    /**
+     * @return true if, and only if, the exception thrown wraps another exception, both of which
+     * are {@link SocketTimeoutException}s, and if the wrapped exception's message's text exactly
+     * matches {@link #READ_TIMEOUT_TEXT}.
+     */
+    public boolean wasReadTimeout() {
+        if (rootCause instanceof SocketTimeoutException) {
+            return Optional.of(getRootCause().getCause())
+                    .filter(cause -> cause instanceof SocketTimeoutException)
+                    .map(Throwable::fillInStackTrace)
+                    .map(Throwable::getMessage)
+                    .map(message -> message.contains(READ_TIMEOUT_TEXT))
+                    .orElse(false);
+        }
+
+        return false;
     }
 }
